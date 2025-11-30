400cdda640d6b7a613dc35101209ed12f108afd8
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2025-11-30 22:47:48 +0100

    initial code

---
+++ test/Token.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+
+contract TestERCDividends is Test {
+    
+
+    function setUp() public {
+
+    }
+
+
+    function test1() public {
+       
+    }
+}
